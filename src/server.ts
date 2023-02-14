06a08c5eb4e1837b84e5cc355b83e56026b1018c
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-14 17:37:31 -0300

    feat: instalação de pacotes necessários para o desenvolvimento do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,15 @@
-import fastify from "fastify";
+import fastify from 'fastify'
 
 const server = fastify()
+
+server.get('/hello', () => {
+  return 'hello node'
+})
+
+server
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running on port')
+  })
