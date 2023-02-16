1618ea01be695d6eb1b004bda9247ce6d4bd5aa2
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-16 16:55:13 -0300

    feat: utilizando cookies no fastify

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,8 @@ import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 import { knex } from '../database'
 
+// Cookies <--> Formas da gente manter contexto entre requisições
+
 export async function transactionsRoutes(server: FastifyInstance) {
   server.get('/', async (request, response) => {
     const transactions = await knex('transactions').select()
@@ -42,10 +44,22 @@ export async function transactionsRoutes(server: FastifyInstance) {
       request.body,
     )
 
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      response.cookie('sessionId', sessionId, {
+        path: '/transactions',
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
       amount: type === 'credit' ? amount : amount * -1,
+      session_id: sessionId,
     })
 
     return response.status(201).send({ success: true })
