03e0073c795ba71e011dd2364e2ef0c4967d95f2
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-16 16:27:35 -0300

    feat: criação de rotas de listagem de transações

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,24 @@ import { z } from 'zod'
 import { knex } from '../database'
 
 export async function transactionsRoutes(server: FastifyInstance) {
+  server.get('/', async (request, response) => {
+    const transactions = await knex('transactions').select()
+
+    return { transactions }
+  })
+
+  server.get('/:id', async (request, response) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = getTransactionParamsSchema.parse(request.params)
+
+    const transaction = await knex('transactions').where('id', id).first()
+
+    return { transaction }
+  })
+
   server.post('/', async (request, response) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
