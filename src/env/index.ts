c4f9a6db32f0a32ec82a5375fa3e9456b14dc961
Alexandre Junior <alexandrejunior408@gmail.com>
2023-02-27 12:10:24 -0300

    chore: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -13,8 +13,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333), // coerce transforma o valor em um número e se isso não retornar um valor válido irá retornar o padrão
 })
 
 const _env = envSchema.safeParse(process.env) // parse dispara um erro automaticamente
